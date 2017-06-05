dfe5f4fee9547a8b7c8c28a0b60b02363ced0999
iamruinous / peterdemartini <jade.meskill@gmail.com , thepeterdemartini@gmail.com>
2017-06-05 10:25:32 -0700

    v1.0.0 basic installer working

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const dashdash = require("dashdash")
 const path = require("path")
+const createPackage = require("@octoblu/osx-pkg")
+const fs = require("fs")
+const chalk = require("chalk")
 
 const CLI_OPTIONS = [
   {
@@ -13,11 +16,25 @@ const CLI_OPTIONS = [
     help: "Print this help and exit.",
   },
   {
-    names: ["platform", "p"],
+    names: ["title"],
     type: "string",
-    env: "MESHBLU_CONNECTOR_INSTALLER_PLATFORM",
-    help: "Platform to build for",
-    helpArg: "PLATFORM",
+    env: "MESHBLU_CONNECTOR_TITLE",
+    help: "Installer title",
+    helpArg: "TITLE",
+  },
+  {
+    names: ["type"],
+    type: "string",
+    env: "MESHBLU_CONNECTOR_TYPE",
+    help: "meshblu connector type",
+    helpArg: "TYPE",
+  },
+  {
+    names: ["deploy-path"],
+    type: "string",
+    env: "MESHBLU_CONNECTOR_DEPLOY_PATH",
+    help: "Deploy path where assets are located. Defaults to current directory/deploy",
+    helpArg: "PATH",
   },
 ]
 
@@ -29,17 +46,24 @@ class MeshbluConnectorInstallerCommand {
     if (!argv) return this.die(new Error("MeshbluConnectorInstallerCommand requires options.argv"))
     this.argv = argv
     this.cliOptions = cliOptions
+    this.parser = dashdash.createParser({ options: this.cliOptions })
   }
 
-  parseArgv({ argv, options }) {
-    var parser = dashdash.createParser({ options })
+  parseArgv({ argv }) {
     try {
-      var opts = parser.parse(argv)
+      var opts = this.parser.parse(argv)
     } catch (e) {
       return {}
     }
+
+    if (!opts.deployPath) {
+      opts.deployPath = path.join(process.cwd(), "deploy")
+    }
+
+    opts.deployPath = path.resolve(opts.deployPath)
+
     if (opts.help) {
-      console.log(`usage: meshblu-connector-installer [OPTIONS]\noptions:\n${parser.help({ includeEnv: true })}`)
+      console.log(`usage: meshblu-connector-installer [OPTIONS]\noptions:\n${this.parser.help({ includeEnv: true })}`)
       process.exit(0)
     }
 
@@ -52,12 +76,34 @@ class MeshbluConnectorInstallerCommand {
   }
 
   run() {
-    const options = this.parseArgv({
-      argv: this.argv,
-      options: this.cliOptions,
-    })
-    const { target } = options
-    if (!target) return this.die(new Error("MeshbluConnectorCommand requires --target or MESHBLU_CONNECTOR_INSTALLER_TARGET"))
+    const options = this.parseArgv({ argv: this.argv })
+    const { type, title, deployPath } = options
+    var errors = []
+    if (!type) errors.push(new Error("MeshbluConnectorCommand requires --type or MESHBLU_CONNECTOR_TYPE"))
+    if (!title) errors.push(new Error("MeshbluConnectorCommand requires --title or MESHBLU_CONNECTOR_TITLE"))
+
+    if (errors.length) {
+      console.log(`usage: meshblu-connector-installer [OPTIONS]\noptions:\n${this.parser.help({ includeEnv: true })}`)
+      errors.forEach(error => {
+        console.error(chalk.red(error.message))
+      })
+      process.exit(1)
+    }
+
+    this.buildInstaller({ type, title, deployPath })
+  }
+
+  buildInstaller({ type, title, deployPath }) {
+    const installerPath = path.join(deployPath, "Installer.pkg")
+    const installLocation = `/Library/MeshbluConnectors/${type}`
+    const opts = {
+      dir: deployPath,
+      installLocation: installLocation,
+      identifier: `.com.octoblu.connectors.${type}.pkg`,
+      title: title,
+    }
+
+    createPackage(opts).pipe(fs.createWriteStream(installerPath))
   }
 
   die(error) {
